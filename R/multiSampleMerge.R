d685a5adbcf941e7b2ab3e6fcb8aa979663623d9
js430 <jeffreyshi430@gmail.com>
2019-08-02 10:34:07 -0400

    Update multiSampleMerge.R

--- R/multiSampleMerge.R
+++ R/multiSampleMerge.R
@@ -17,17 +17,15 @@ multiSampleMerge <-function(orderVec){
     temp = list.files(pattern="*")
     #temp<-temp[seq(1, length(temp), 3)]
     myfiles = lapply(temp, fread)
-    new<-bind_cols(myfiles)
+    new<-bind_rows(myfiles)
     new<- new %>% remove_rownames %>% column_to_rownames(var="Position") #tidyverse
-    new<-new[c(T,F)]
     new<-order(new, orderVec)
     name<-substr(temp[1], 1, 12)
     new<-`colnames<-`(new, name)
-    fwrite(new, name)
 }
 order<-function(dataFrame, orderVector){
     dataFrame<-dataFrame[,orderVector]
-    final<-apply(new, 1, function(x) if(all(is.na(x))) NA else x[!is.na(x)][1])
-    finaldf<-data.frame(final)
+    final<-apply(new, 1, function(x) if(is.na(x)) NA else x[!is.na(x)][1])
+    finald<-data.frame(final)
     return(finaldf)
 }
